0ccdabd779cc01a1259546509cbde9105deba84b
Marina <3funky3@rambler.ru>
2021-04-19 17:51:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use sсtrict";
+//alert('Hello');
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = +prompt("Вам есть 18?", "");
+//console.log(answer + 5);
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Как ваши дела?', '');
+
+document.write(answers);
---
+++ js/script1.js
@@ -0,0 +1,30 @@
+"use sсtrict";
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+var name = 'Ivan';
+
+console.log(4/0);
+console.log('string' * 9);
+const person = "Alex";
+
+const bool = true;
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+//console.log(obj["name"]); //так тоже можно иногда
+
+let arr = ['plum.png', 'orange.jpg', 6, [], {}, 'apple.bmp'];
+console.log(arr[1]);
