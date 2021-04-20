5dc432acd903cf724f074a2acc952a2bd8755f50
Marina <3funky3@rambler.ru>
2021-04-20 17:59:18 +0300

    update script3

--- js/script3.js
+++ js/script3.js
@@ -2,3 +2,20 @@
 
 console.log('arr' + " - object");
 console.log(5 + " - object");
+console.log(5 + "4");
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+console.log(++incr);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != 8);
+
+const isCheked = false,
+      isClosed = false;
+console.log(isCheked || !isClosed);
